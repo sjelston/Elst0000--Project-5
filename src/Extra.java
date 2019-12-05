e4d5f6afc0d64b4d4991e1426fdbf9de2c3fd621
Sommer Elston <sommerjelston@gmail.com>
2019-12-05 12:14:54 -0600

    working on extra

--- src/Extra.java
+++ src/Extra.java
@@ -1,3 +1,5 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
@@ -11,7 +13,17 @@ public class Extra {
 	GridPane g = new GridPane();
 	
 	Button A = new Button("Which");
-	A.setOnAction(ActionEvent);
+	A.setOnAction(new EventHandler<ActionEvent>() {
+		MesoStation current = new MesoStation(inputTxt.getText());
+
+		@Override
+		public void handle(ActionEvent e) {
+			if (!(dropDown.getItems().contains(current))) {
+				dropDown.getItems().add(current);
+				dropDown.getItems().sort(current);
+			}
+		}
+	});
 	Button B = new Button("Will");
 	Button C = new Button("You");
 	Button D = new Button("Choose?");
